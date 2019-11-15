8138b0ec610d0ebedd041eb961169ae9f50bcd3b
Van Thanh <36842034+ThanhTrinhVan@users.noreply.github.com>
2019-11-15 13:35:29 +0300

    Add files via upload

--- Code/PillarObject.cpp
+++ Code/PillarObject.cpp
@@ -20,10 +20,12 @@ void PillarObject::draw(int pX, int pY, SDL_Renderer * des)
 	// draw
 	height += 5;
 	posX = pX;
-	posY = pY - height;
-	SDL_Rect fill_rect = { posX, posY, width, height };
+	posY = pY;
 	SDL_SetRenderDrawColor(des, (Uint8) 80, (Uint8) 80, (Uint8) 150, 0xFF);
-	SDL_RenderFillRect(des, &fill_rect);
+	SDL_RenderDrawLine(des, posX, posY - height, posX, posY);
+	SDL_RenderDrawLine(des, posX, posY - height, posX + width, posY - height);
+	SDL_RenderDrawLine(des, posX, posY, posX + width, posY);
+	SDL_RenderDrawLine(des, posX + width, posY, posX + width, posY - height);
 }
 
 void PillarObject::handelInputAction(SDL_Event events, SDL_Renderer * des)
@@ -34,12 +36,23 @@ void PillarObject::handelInputAction(SDL_Event events, SDL_Renderer * des)
 		}
 	}
 	if (events.type == SDL_KEYUP) {
-		if (events.key.keysym.sym == SDLK_SPACE)
+		if (events.key.keysym.sym == SDLK_SPACE) {
 			recline(des);
+		}
 	}
 }
 
 void PillarObject::recline(SDL_Renderer * des)
 {
+	int numStep = 5;
+	for (int i = 0; i < numStep; i++) {
+		double angle = i * pi / (2.0*5.0);
+		SDL_SetRenderDrawColor(des, (Uint8)80, (Uint8)80, (Uint8)150, 0xFF);
+		SDL_RenderDrawLine(des, posX, posY, posX + height * sin(angle), posY - height * cos(angle));
+		SDL_RenderDrawLine(des, posX, posY, posX + width * cos(angle), posY + width * sin(angle));
+		SDL_RenderDrawLine(des, posX + width * cos(angle), posY + width * sin(angle), posX + height * sin(angle) + width * cos(angle), posY - height * cos(angle) + width * sin(angle));
+		SDL_RenderDrawLine(des, posX + height * sin(angle), posY - height * cos(angle), posX + height * sin(angle) + width * cos(angle), posY - height * cos(angle) + width * sin(angle));
+		SDL_Delay(1000);
+	}
 	height = 0;
 }
